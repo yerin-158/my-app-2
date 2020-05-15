76c9022127d8965c4bb5740a3c8918288ba7b49d
robin.son <robin.son@kakaocorp.com>
2020-05-15 21:05:01 +0900

    'O'와 'X'를 변경하여 출력 할 수 있도록 상태 추가

--- src/views/components/Board.js
+++ src/views/components/Board.js
@@ -2,22 +2,33 @@ import React, {useEffect, useState} from "react";
 import Square from "./Square.js";
 
 const Board = ({init}) => {
-    const status = 'Next player: X';
+    const status = 'Next player: ';
     const [squares, setSquares] = useState(init);
+    const [xIsNext, setXIsNext] = useState(true);
 
     const handleClick = (event) => {
+        setSquares(getNewSquares(event));
+        setXIsNext(!xIsNext);
+    }
+
+    const getNewSquares = (event) => {
+        const [row, column] = getLocation(event);
+        var newSquares = squares.slice();
+        // eslint-disable-next-line no-unused-expressions
+        newSquares[row][column].value = (xIsNext ? 'X' : 'O')
+        return newSquares;
+    }
+
+    const getLocation = (event) => {
         var index = parseInt(event.target.dataset.index);
         var row = parseInt(index / squares[0].length);
         var column = index - squares[0].length * row;
-        var newSquares = squares.slice();
-        // eslint-disable-next-line no-unused-expressions
-        newSquares[row][column].value = 'X';
-        setSquares(newSquares);
+        return [row, column];
     }
 
     return (
         <div>
-            <div className="status">{status}</div>
+            <div className="status">{status + (xIsNext ? 'X' : 'O')}</div>
             {squares.map(row =>
                 <div className="board-row">
                     {row.map(column =>
