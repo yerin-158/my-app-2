2d9597cce52a08ce0fca2df3c4504c3de84c76ae
robin.son <robin.son@kakaocorp.com>
2020-05-17 18:24:10 +0900

    승자를 결정하는 코드 추가

--- src/views/components/Board.js
+++ src/views/components/Board.js
@@ -2,13 +2,47 @@ import React, {useEffect, useState} from "react";
 import Square from "./Square.js";
 
 const Board = ({init}) => {
-    const status = 'Next player: ';
+    const status = 'Next Player : ';
     const [squares, setSquares] = useState(init);
     const [xIsNext, setXIsNext] = useState(true);
+    const [winner, setWinner] = useState(null);
+
+
+    useEffect(() => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (let i = 0; i < lines.length; ++i) {
+            const [a, b, c] = lines[i];
+            if (isWin(getLocation(a), getLocation(b), getLocation(c))) {
+                setWinner(xIsNext ? 'O' : 'X');
+                break;
+            }
+        }
+    }, [squares]);
+
+    const isWin = (...locations) => {
+        for (let i = 1; i < locations.length; ++i) {
+            const [rowNow, colNow] = locations[i - 1];
+            const [rowNext, colNext] = locations[i];
+            if (squares[rowNow][colNow].value == null || squares[rowNow][colNow].value !== squares[rowNext][colNext].value) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     const handleClick = (event) => {
-        const [row, column] = getLocation(event);
-        if ( squares[row][column].value == null ){
+        const [row, column] = getLocation(event.target.dataset.index);
+        if (squares[row][column].value == null) {
             setSquares(getNewSquares(row, column));
             setXIsNext(!xIsNext);
         }
@@ -20,8 +54,7 @@ const Board = ({init}) => {
         return newSquares;
     }
 
-    const getLocation = (event) => {
-        var index = parseInt(event.target.dataset.index);
+    const getLocation = (index) => {
         var row = parseInt(index / squares[0].length);
         var column = index - squares[0].length * row;
         return [row, column];
@@ -29,7 +62,10 @@ const Board = ({init}) => {
 
     return (
         <div>
-            <div className="status">{status + (xIsNext ? 'X' : 'O')}</div>
+            {winner == null ?
+                <div className="status">{status + (xIsNext ? 'X' : 'O')}</div> :
+                <div className="status">{'Winner is ' + winner + ' !!!'}</div>
+            }
             {squares.map(row =>
                 <div className="board-row">
                     {row.map(column =>
