25357bb5eed7438d29ff9681c690614be154924f
robin.son <robin.son@kakaocorp.com>
2020-05-18 15:32:22 +0900

    리듀서 추가

---
+++ src/views/store/modules/reducers.js
@@ -0,0 +1,46 @@
+import {handleActions} from 'redux-actions'
+import {type} from './types';
+
+const initializeSquares = () => {
+    const MAXIMUM_ROW = 3, MAXIMUM_COLUMN = 3;
+    const init = Array(MAXIMUM_ROW).fill(null);
+    for (var i = 0; i < MAXIMUM_ROW; ++i) {
+        var columns = Array(MAXIMUM_COLUMN).fill(null);
+        for (var j = 0; j < MAXIMUM_COLUMN; ++j) {
+            columns[j] = {index: i * MAXIMUM_COLUMN + j, value: null};
+        }
+        init[i] = columns;
+    }
+    return [{squares: init}];
+}
+
+const initialState = {
+    history : initializeSquares(),
+    xIsNext : true,
+    clickCount : 0,
+    status : 'Next Player : X'
+}
+
+export default handleActions({
+    [type.SQUARE_CLICK]: (state, action) => ({
+        ...state,
+        history: state.history.concat({squares : action.payload}),
+        status: 'Next Player : '+(state.xIsNext ? 'O' : 'X'),
+        xIsNext: !state.xIsNext,
+        clickCount: state.clickCount+1,
+    }),
+    [type.GO_TO_HISTORY]: (state, action) => ({
+        ...state,
+        history: state.history.slice(0, action.payload + 1)
+    }),
+    [type.WINNER_DETERMINED]: (state, action) => ({
+        ...state,
+        status: 'Winner is '+(state.xIsNext ? 'O' : 'X')
+    }),
+    [type.DRAW_ENDS]: (state, action) => ({
+        ...state,
+        status : 'GAME-OVER'
+    })
+  },
+  initialState
+);
