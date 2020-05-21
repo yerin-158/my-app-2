405f1833aea5176925c3ca4e6e22cf01ba41ab4b
robin.son <robin.son@kakaocorp.com>
2020-05-21 16:36:36 +0900

    store 연결

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore } from 'redux';
+import rootReducer from './views/store/modules/reducers';
+
+import { Provider } from 'react-redux';
+
 import Game from './views/components/Game';
 import './index.css';
+import BoardContainer from "./views/containers/BoardContainer";
+
+const store = createStore(rootReducer);
+console.log(store.getState());
 
 ReactDOM.render(
-    <Game />,
+    <Provider store={store}>
+        <BoardContainer />
+    </Provider>,
     document.getElementById('root')
 );
